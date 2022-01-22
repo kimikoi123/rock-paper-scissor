abb20f09f4d52a1931b473e025b9e2f061430c54
kimikoi123 <kimuelanqui3@gmail.com>
2022-01-22 12:49:23 +0800

    Initial Update

---
+++ app.js
@@ -0,0 +1,74 @@
+
+
+var userCount = 0
+var computerCount = 0
+let scissorDiv = document.getElementsByClassName('choice-s')[0]
+let paperDiv = document.getElementsByClassName('choice-p')[0]
+let rockDiv = document.getElementsByClassName('choice-r')[0]
+let userScore = document.getElementsByClassName('user-score')[0]
+let computerScore = document.getElementsByClassName('computer-score')[0]
+
+
+function win() {
+    userCount++
+    userScore.innerHTML = userCount
+    if (userCount === 5) {
+        alert('You win!')
+        location.reload()
+    }
+}
+
+function lose() {
+    computerCount++
+    computerScore.innerHTML = computerCount
+    if (computerCount === 5) {
+        alert('You lose!')
+        location.reload()
+    }
+}
+
+function draw() {
+    computerScore.innerHTML = computerCount
+    userScore.innerHTML = userCount
+}
+
+
+
+
+function computerPlay() {
+    let choices = ['r', 'p', 's']
+    let randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber]
+
+}
+
+function playGame(playerSelection) {
+    let computerSelection = computerPlay()
+    switch (playerSelection + computerSelection) {
+        case 'sp':
+        case 'pr':
+        case 'rs':
+            win()
+            break;
+        case 'sr':
+        case 'ps':
+        case 'rp':
+            lose()
+            break;
+        case 'ss':
+        case 'pp':
+        case 'rr':
+            draw()
+            break;
+    }
+}
+
+
+
+function game() {
+    paperDiv.addEventListener('click', () => playGame('p'))
+    scissorDiv.addEventListener('click',() => playGame('s'))
+    rockDiv.addEventListener('click', () => playGame('r'))
+}
+
+game()
